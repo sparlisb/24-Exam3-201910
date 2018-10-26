2f1c29467005d728836a6fd5bf23c919d0c41ac3
sparlisb <sparlisb@rose-hulman.edu>
2018-10-25 21:32:42 -0400

    finished problem 1, 2

--- src/problem2.py
+++ src/problem2.py
@@ -2,9 +2,9 @@
 Exam 3, problem 2.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  October 2018.
+         their colleagues and Shamus Sparling.  October 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -90,9 +90,25 @@ def problem2(sequence):
       :type sequence [list]
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # done: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    l = sequence[0]
+    x = 0
+    for k in range(len(sequence)):
+        if sequence[k] > l:
+            if l < 0:
+                if sequence[k] > -l:
+                    l = sequence[k]
+                    x = k
+            else:
+                l = sequence[k]
+                x = k
+        elif sequence[k] < 0:
+            if sequence[k] < -l:
+                l = sequence[k]
+                x = k
+    return x
 
 
 ###############################################################################
