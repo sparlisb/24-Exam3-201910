c06ab9b92393708588f0413e45b3f1a882aaf094
sparlisb <sparlisb@rose-hulman.edu>
2018-10-25 21:38:32 -0400

    finished problem 1, 2, 3

--- src/problem3.py
+++ src/problem3.py
@@ -2,8 +2,8 @@
 Exam 3, problem 3.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, their colleagues,
-         and PUT_YOUR_NAME_HERE.  October, 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Shamus Sparling.  October, 2018.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import testing_helper
 
@@ -223,10 +223,15 @@ def problem3(seq_of_seq):
       :rtype: (list of int) | int
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # done: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
-
+    for k in range(len(seq_of_seq)):
+        seq = seq_of_seq[k]
+        for m in range(len(seq)):
+            if is_prime(seq[m]) == True:
+                return seq_of_seq[k]
+    return -1
 
 ###############################################################################
 # Our tests use the following to print error messages in red.
