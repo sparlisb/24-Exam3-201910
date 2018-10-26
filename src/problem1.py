9bec09f4c1a929b870dfe98b4add2709937eb887
sparlisb <sparlisb@rose-hulman.edu>
2018-10-25 21:21:43 -0400

    finished problem 1

--- src/problem1.py
+++ src/problem1.py
@@ -2,9 +2,9 @@
 Exam 3, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  October 2018.
+         their colleagues and Shamus Sparling.  October 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -86,9 +86,16 @@ def problem1(numbers):
       :type numbers:  [int]
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # done: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    total = 0
+    n = len(numbers) / 3
+    for k in range(len(numbers)):
+        if k >= n:
+            if k < 2*n:
+                total = total + numbers[k]
+    return total
 
 
 ###############################################################################
